7ca228d095811ffab691af3410cb152d904985e8
Aarushi Gupta <aarushigupta55555@gmail.com>
2020-05-18 13:55:37 +0530

    Update with subject of mail

--- send_mail.py
+++ send_mail.py
@@ -1,10 +1,16 @@
 import smtplib
-message = "So this is a test message"
+senders_address = "test@domain.com"
+receivers_address = "address@domain.com"
+message = (f"From: {senders_address}\n"
+           f"To: {receivers_address}\n"
+           f"Subject: SMTP e-mail test\n"
+           f"\n"
+           f"This is a test e-mail message.\n")
 mail = smtplib.SMTP("smtp.gmail.com", 587)
 mail.ehlo()  # command to SMTP server to identify itself (just like saying hello)
 mail.starttls()   # start transfer layer security
 # The above command helps secure the below login details
-mail.login("senders_address", "password")
-mail.sendmail("senders_address", "receivers_address", message)
-print ("Successfully sent email")
+mail.login(senders_address, "password")
+mail.sendmail(senders_address, receivers_address, message)
+print(f"Successfully sent email to {receivers_address}")
 mail.close()

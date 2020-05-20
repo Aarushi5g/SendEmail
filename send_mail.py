914ae6135959b2d284567505d75661c6fc932519
Aarushi Gupta <aarushigupta55555@gmail.com>
2020-05-20 19:03:44 +0530

    Simple mailing application

--- send_mail.py
+++ send_mail.py
@@ -1,35 +1,25 @@
 import smtplib
 from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
-from email.mime.base import MIMEBase
-from email import encoders
 
-users_email = 'domain@gmail.com'
-receivers_email = 'email@gmail.com'
+users_email = input("Enter your mail: ")
+password = input("Enter your password: ")
+receivers_email = input("Mail to? ")
 
-subject = 'subject'
+subject = input("Enter the subject of the mail: ")
 
 msg = MIMEMultipart()
 msg['From'] = users_email
 msg['To'] = receivers_email
 msg['Subject'] = subject
 
-body = 'Hi there, sending this email from Python!'
+body = input("Enter the body of the mail: ")
 msg.attach(MIMEText(body, 'plain'))
 
-filename = 'file_path'
-attachment = open(filename, 'rb')
-
-part = MIMEBase('application','octet-stream')
-part.set_payload((attachment).read())
-encoders.encode_base64(part)
-part.add_header('Content-Disposition', "attachment; filename= "+filename)
-
-msg.attach(part)
 text = msg.as_string()
 server = smtplib.SMTP('smtp.gmail.com',587)
 server.starttls()
-server.login(users_email, "password")
+server.login(users_email, password)
 
 
 server.sendmail(users_email, receivers_email, text)

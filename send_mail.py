abb163a448241cdccb6cbbacdd2166f4c07f1760
Aarushi Gupta <aarushigupta55555@gmail.com>
2020-05-20 18:53:34 +0530

    Send a mail with attachment

--- send_mail.py
+++ send_mail.py
@@ -1,8 +1,10 @@
 import smtplib
 from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
+from email.mime.base import MIMEBase
+from email import encoders
 
-users_email = 'address@gmail.com'
+users_email = 'domain@gmail.com'
 receivers_email = 'email@gmail.com'
 
 subject = 'subject'
@@ -14,11 +16,22 @@ msg['Subject'] = subject
 
 body = 'Hi there, sending this email from Python!'
 msg.attach(MIMEText(body, 'plain'))
-text = msg.as_string()
 
-mail = smtplib.SMTP('smtp.gmail.com', 587)
-mail.starttls()
-mail.login(users_email, "password")
-mail.sendmail(users_email, receivers_email, text)
-mail.quit()
+filename = 'file_path'
+attachment = open(filename, 'rb')
+
+part = MIMEBase('application','octet-stream')
+part.set_payload((attachment).read())
+encoders.encode_base64(part)
+part.add_header('Content-Disposition', "attachment; filename= "+filename)
+
+msg.attach(part)
+text = msg.as_string()
+server = smtplib.SMTP('smtp.gmail.com',587)
+server.starttls()
+server.login(users_email, "password")
+
+
+server.sendmail(users_email, receivers_email, text)
+server.quit()
 print(f"Sent a mail to {receivers_email}")
